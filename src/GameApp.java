89a50565f62a85276e57813b025a8c11ef36af2d
stevenG916 <stevenscottgraham916@gmail.com>
2022-10-29 20:30:41 -0700

    Added all the starting info from A1 - RainMaker Specifications

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,10 +1,59 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class GameApp extends Application {
+    private static final int GAME_WIDTH = 400;
+    private static final int GAME_HEIGHT = 800;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
 
+        Group root = new Group();
+        Scene scene = new Scene(root, GAME_WIDTH, GAME_HEIGHT);
+        stage.setTitle("RainMaker!");
+        stage.setScene(scene);
+        stage.show();
     }
+    public static void main(String[] args) {
+        launch(args);
+    }
+} // end of class GameApp
+
+
+class Game extends Pane{
+
+
+}
+abstract class GameObject extends Group{
+
+}
+class Pond extends GameObject{
+
+}
+class Cloud extends GameObject{
+
+}
+class Helipad{
+
+}
+class Helicopter{
+
+}
+
+interface Updatable{
+    void update();
+}
+class GameText{
+
 }
