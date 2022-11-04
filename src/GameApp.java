4dde078849e4d6b0f7326472d47c427459a7ccec
StevenG916 <92830236+StevenG916@users.noreply.github.com>
2022-11-04 15:48:51 -0700

    Just a little modification, I added the start of the controls, though i should really make the objects first.

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,7 +1,11 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -10,8 +14,13 @@ import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+
+interface Updatable{
+    void update();
+}
 public class GameApp extends Application {
     private static final int GAME_WIDTH = 400;
     private static final int GAME_HEIGHT = 800;
@@ -20,10 +29,34 @@ public class GameApp extends Application {
     public void start(Stage stage) throws Exception {
 
         Group root = new Group();
+        init();
         Scene scene = new Scene(root, GAME_WIDTH, GAME_HEIGHT);
         stage.setTitle("RainMaker!");
         stage.setScene(scene);
+        root.setScaleY(-1);
         stage.show();
+
+        scene.setOnKeyPressed((new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.LEFT){
+
+                }
+                if (event.getCode() == KeyCode.RIGHT){
+
+                }
+                if (event.getCode() == KeyCode.UP){
+
+                }
+                if (event.getCode() == KeyCode.DOWN){
+
+                }
+                if (event.getCode() == KeyCode.I){
+
+                }
+            }
+        }));
+
     }
     public static void main(String[] args) {
         launch(args);
@@ -35,14 +68,24 @@ class Game extends Pane{
 
 
 }
-abstract class GameObject extends Group{
+abstract class GameObject extends Group implements Updatable {
+    void add (Node node) {
+        this.getChildren().add(node);
 
 }
 class Pond extends GameObject{
 
+    @Override
+    public void update() {
+
+    }
 }
 class Cloud extends GameObject{
 
+    @Override
+    public void update() {
+
+    }
 }
 class Helipad{
 
@@ -51,9 +94,20 @@ class Helicopter{
 
 }
 
-interface Updatable{
-    void update();
-}
-class GameText{
 
-}
+class GameText extends GameObject implements Updatable{
+
+    private Text text;
+    public GameText(String text, Color color) {
+        this.text = new Text(text);
+        this.text.setScaleY(-1);
+        this.text.setFill(color);
+        this.text.setFont(Font.font(20));
+        this.getChildren().add(this.text);
+    }
+
+    @Override
+    public void update() {
+
+    }
+};}
