f3fdc142c27d6cce7f32e09b1f45482884c01d68
StevenG916 <92830236+StevenG916@users.noreply.github.com>
2022-11-07 21:33:38 -0800

    Got a lot more done, but not where its supposed to be at.

--- src/GameApp.java
+++ src/GameApp.java
@@ -4,27 +4,27 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
+import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 
 interface Updatable{
     void update();
 }
+
 public class GameApp extends Application {
     private static final int GAME_WIDTH = 400;
     private static final int GAME_HEIGHT = 800;
@@ -32,12 +32,18 @@ public class GameApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
+
         Group root = new Group();
+        Game game = new Game();
+        root.setScaleY(-1);
         init(root);
+        root.getChildren().add(game);
         Scene scene = new Scene(root, GAME_WIDTH, GAME_HEIGHT);
         stage.setTitle("RainMaker!");
         stage.setScene(scene);
-        root.setScaleY(-1);
+
+        stage.show();
+
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
@@ -69,13 +75,13 @@ public class GameApp extends Application {
             }
 
         }));
-        stage.show();
+
         timer.start();
 
     }
 
     private void init(Group root) {
-        //root.getChildren().add(); This is where to add them, just have to make them first.
+        //root.getChildren().add(); //This is where to add them, just have to make them first.
     }
 
     public static void main(String[] args) {
@@ -84,7 +90,18 @@ public class GameApp extends Application {
 } // end of class GameApp
 
 
-class Game extends Pane{
+class Game extends Pane {
+
+    public Game() {
+        Helipad helipad = new Helipad();
+        Helicopter helicopter = new Helicopter();
+        Pond pond = new Pond();
+        Cloud cloud = new Cloud();
+
+        getChildren().addAll(helipad, helicopter, pond, cloud);
+
+
+    }
 
 
 }
@@ -130,9 +147,25 @@ abstract class GameObject extends Group implements Updatable {
 
     void add (Node node) {
         this.getChildren().add(node);
+    }
 
 }
-class Pond extends GameObject{
+class Pond extends GameObject implements Updatable{
+    public Pond() {
+        double r = Math.random() * 40 + 30;
+        Ellipse ellipse = new Ellipse(r, r*1.5);
+        Random rand = new Random();
+        ellipse.setFill(Color.BLUE);
+        add(ellipse);
+        double randX = rand.nextDouble();
+        double randY = rand.nextDouble();
+        ellipse.setTranslateX(randX*50);
+        ellipse.setTranslateY(randY*50);
+        translate(100,100  );
+
+
+
+    }
 
     @Override
     public void update() {
@@ -146,15 +179,29 @@ class Cloud extends GameObject{
 
     }
 }
-class Helipad{
+class Helipad extends GameObject{
+
+    public Helipad() {
+
+        Circle circle = new Circle(47.5);
+        Rectangle rectangle = new Rectangle(100, 100);
+        rectangle.setFill(Color.GREY);
+        circle.setFill(Color.GRAY);
+        rectangle.setStroke(Color.BLACK);
+        circle.setStroke(Color.BLACK);
+        add(rectangle);
+        add(circle);
+        circle.setTranslateX(50);
+        circle.setTranslateY(50);
+        translate(150, 20);
+    }
 
 }
-class Helicopter{
+class Helicopter extends GameObject implements Updatable {
 
 }
 
-
-class GameText extends GameObject implements Updatable{
+class GameText extends GameObject implements Updatable {
 
     private Text text;
     public GameText(String text, Color color) {
@@ -169,4 +216,4 @@ class GameText extends GameObject implements Updatable{
     public void update() {
 
     }
-};}
+}
